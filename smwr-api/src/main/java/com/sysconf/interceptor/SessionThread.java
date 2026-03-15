6ec776ec5657ef909e267a73e8e55237e2e72a9f
jgh9514 <40375824+jgh9514@users.noreply.github.com>
2026-03-15 23:17:05 +0900

    롤백

--- smwr-api/src/main/java/com/sysconf/interceptor/SessionThread.java
+++
@@ -1,7 +0,0 @@
-package com.sysconf.interceptor;
-
-import java.util.Map;
-
-public class SessionThread {
-    public static ThreadLocal<Map<String, Object>> SESSION_USER_INFO = new ThreadLocal<Map<String, Object>>();
-}
