ce876deaa49d445e9f24bdf5cf36ecf3cca50416
jgh9514 <40375824+jgh9514@users.noreply.github.com>
2026-04-25 23:25:48 +0900

    .

--- src/main/java/com/smw/rta/cache/RtaRedisCacheWarmup.java
+++ src/main/java/com/smw/rta/cache/RtaRedisCacheWarmup.java
@@ -42,6 +42,8 @@ public class RtaRedisCacheWarmup {
 			rtaService.getRtaSummonerRanking(50, 0, sid, null);
 			rtaService.getRtaListPage(20, 0, sid, null, null);
 			rtaService.getRtaMonsterStats(30, 0, "solo", sid, null, null);
+			rtaService.getRtaMonsterStats(30, 0, "duo", sid, null, null);
+			rtaService.getRtaMonsterStats(30, 0, "trio", sid, null, null);
 			rtaService.getRtaStats(sid, null, null);
 			log.debug("[rta-cache] Redis warmup finished for seasonId={}", sid);
 		} catch (Exception e) {
